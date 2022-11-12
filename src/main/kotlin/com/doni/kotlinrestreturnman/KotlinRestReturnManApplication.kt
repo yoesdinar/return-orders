fa5a25ae081d4c3ccb21caffdd04ebfb9a66e451
Doni Yusdinar <doni.yusdinar@gdplabs.id>
2022-11-12 23:08:04 +0700

    initial project init

---
+++ src/main/kotlin/com/doni/kotlinrestreturnman/KotlinRestReturnManApplication.kt
@@ -0,0 +1,11 @@
+package com.doni.kotlinrestreturnman
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinRestReturnManApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinRestReturnManApplication>(*args)
+}
---
+++ src/test/kotlin/com/doni/kotlinrestreturnman/KotlinRestReturnManApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.doni.kotlinrestreturnman
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRestReturnManApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
